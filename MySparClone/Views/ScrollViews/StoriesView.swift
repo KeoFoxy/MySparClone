e73050cdede670d309852cca33c41eebd527bd70
KeoFoxy <arasdiel68@gmail.com>
2023-08-10 23:00:10 +0300

    Renamed

---
+++ MySparClone/Views/ScrollViews/StoriesView.swift
@@ -0,0 +1,30 @@
+//
+//  StoriesView.swift
+//  MySparClone
+//
+//  Created by Alexander Sorokin on 10.08.2023.
+//
+
+import SwiftUI
+
+struct StoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 5) {
+                CircleComponent()
+                CircleComponent(imageName: "telegram", bannerText: "Мы в соцсетях")
+                CircleComponent(imageName: "cocktail", bannerText: "3 рецепта коктейлей")
+                CircleComponent(imageName: "tasting", bannerText: "Дегустаци в SPAR")
+                CircleComponent(imageName: "drinks", bannerText: "Лучший квас")
+                
+            }
+            .padding(.top, 5)
+        }
+    }
+}
+
+struct StoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoriesView()
+    }
+}
