5e635b1154f4bda3347ed5c0ebf397f25127ece0
KeoFoxy <arasdiel68@gmail.com>
2023-08-10 20:03:21 +0300

    Header

---
+++ MySparClone/Views/Header.swift
@@ -0,0 +1,41 @@
+//
+//  Header.swift
+//  MySparClone
+//
+//  Created by Alexander Sorokin on 10.08.2023.
+//
+
+import SwiftUI
+
+struct Header: View {
+    @State private var locationInput: String = ""
+
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "mappin")
+                    .foregroundColor(.red)
+                
+                Text("Москва, Москва и Московская область")
+                    .font(.system(size: 16, weight: .regular))
+                    .lineLimit(1)
+            }
+            .padding(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(Color(.systemGray4), lineWidth: 1)
+            )
+            Image(systemName: "line.3.horizontal")
+                .foregroundColor(Color(.systemGreen))
+                .bold()
+                .imageScale(.large)
+        }
+    }
+}
+
+struct Header_Previews: PreviewProvider {
+    static var previews: some View {
+        Header()
+            .padding()
+    }
+}
