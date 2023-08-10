d3a1e9651e016f3581d1813e4e35755d6752670c
KeoFoxy <arasdiel68@gmail.com>
2023-08-10 19:34:31 +0300

    Initial Commit

---
+++ MySparClone/MySparCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparCloneApp.swift
+//  MySparClone
+//
+//  Created by Alexander Sorokin on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySparCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
